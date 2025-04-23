670adf1f516617559289c1720c146e7175aa7efc
Jonathan Ly <info@gamescapesf.com>
2025-04-23 09:28:31 -0700

    Fix width and added month title

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ $(document).ready(function () {
 	const $slider = $('.slider');
 	const now = new Date();
 	const month = now.getMonth() + 1;
+	const displayMonth = now.getMonth()
 	const year = now.getFullYear();
+
+	const monthNames = [
+		"January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"
+	];
+	const formattedMonth = monthNames[displayMonth].toUpperCase();
+	$('#month-title').text(`${formattedMonth}'S STAFF REVIEWS`);
 
 	function getResponsiveSettings(cardCount) {
 		const breakpoints = [
@@ -51,11 +59,40 @@ $(document).ready(function () {
 		autoplaySpeed: 8000,
 		centerMode: true,
 		centerPadding: '0px',
-		arrows: false,
 		dots: true,
 		mobileFirst: true,
-		slidesToShow: 1,
-		responsive: []
+		responsive: [
+			{
+			  breakpoint: 1440,
+			  settings: {
+				arrows: true,
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			  },
+			},
+			{
+			  breakpoint: 1024,
+			  settings: {
+				arrows:true,
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			  },
+			}
+		  ],
+		  prevArrow: `
+			<button type="button" class="slick-prev custom-arrow">
+			  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="white" stroke-width="2" viewBox="0 0 24 24">
+				<path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z" />
+			  </svg>
+			</button>
+		  `,
+		  nextArrow: `
+			<button type="button" class="slick-next custom-arrow">
+			  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="white" stroke-width="2" viewBox="0 0 24 24">
+				<path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/>
+			  </svg>
+			</button>
+		  `
 	});
 
 	// STEP 3: Fetch reviews and populate Slick
